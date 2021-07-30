17db7514e98d73c60774e334cde5c0ed29830c41
HironoBaba <h-baba@alterbooth.com>
2021-07-30 10:50:45 +0900

    [update] hands_onCRUDページ追加

---
+++ Web/Pages/hands_on/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entities;
+using Web.Data;
+
+namespace Web.Pages.hands_on
+{
+    public class EditModel : PageModel
+    {
+        private readonly Web.Data.WebContext _context;
+
+        public EditModel(Web.Data.WebContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Message Message { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Message = await _context.Message.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Message == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Message).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(Message.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MessageExists(Guid id)
+        {
+            return _context.Message.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Web/Pages/hands_on/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entities;
+using Web.Data;
+
+namespace Web.Pages.hands_on
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Web.Data.WebContext _context;
+
+        public IndexModel(Web.Data.WebContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Message> Message { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Message = await _context.Message.ToListAsync();
+        }
+    }
+}
